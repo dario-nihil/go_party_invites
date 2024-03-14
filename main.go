b346105c9d8e70c157ae973421515698b78e04f8
dario-nihil <dario.nihil@gmail.com>
2024-03-14 20:24:22 +0100

    start implementation of root and list handler functions, and setting up basic server

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"net/http"
 )
 
 type RSVP struct {
@@ -14,10 +15,35 @@ var responses = make([]*RSVP, 0, 10)
 var templates = make(map[string]*template.Template, 3)
 
 func main() {
-	fmt.Println("TODO: add some features")
 	loadTemplates()
+
+	// specifies the URL path and the handler
+	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/list", listHandler)
+
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func loadTemplates() {
-	//TODO - load templates here
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"}
+	for idx, name := range templateNames {
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil {
+			templates[name] = t
+			fmt.Println("Loaded template", idx, name)
+		} else {
+			panic(err)
+		}
+	}
+}
+
+func welcomeHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["welcome"].Execute(writer, nil)
+}
+
+func listHandler(writer http.ResponseWriter, request *http.Request) {
+	templates["list"].Execute(writer, responses)
 }
