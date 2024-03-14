31f97ff748db7e0ce4702b2bc59f1e0d101bad81
dario-nihil <dario.nihil@gmail.com>
2024-03-14 18:21:52 +0100

    created layout templates

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+)
+
+type RSVP struct {
+	Name, Email, Phone string
+	WillAttend         bool
+}
+
+var responses = make([]*RSVP, 0, 10)
+var templates = make(map[string]*template.Template, 3)
 
 func main() {
 	fmt.Println("TODO: add some features")
+	loadTemplates()
+}
+
+func loadTemplates() {
+	//TODO - load templates here
 }
